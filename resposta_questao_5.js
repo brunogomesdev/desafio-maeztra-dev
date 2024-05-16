e67d656243ae83d632e31a402ea531ae71aad210
brunogomesdev <brunowd@gmail.com>
2024-05-16 01:26:06 -0300

    feat(at05): function calculate amount of variation for the photo

--- resposta_questao_5.js
+++ resposta_questao_5.js
@@ -1,1 +1,12 @@
+function organizarPessoas(numeroPessoas) {
+    if (numeroPessoas === 0) {
+        return 1; // Um único arranjo possível com 0 pessoas (ninguém na foto).
+    } else {
+        return numeroPessoas * organizarPessoas(numeroPessoas - 1); // Fórmula fatorial de N para calcular a quantidade de permutações
+    }
+}
 
+const numeroDePessoas = 5;
+const possibilidades = organizarPessoas(numeroDePessoas);
+
+console.log(`Com ${numeroDePessoas} pessoas, existem ${possibilidades} modos distintos de se organizarem para as fotos.`);
