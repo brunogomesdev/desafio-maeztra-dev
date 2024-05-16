8ffe4fed6810d9521c2fd321df1f97ca4724cd3a
brunogomesdev <brunowd@gmail.com>
2024-05-16 01:00:49 -0300

    feat(at01): function to receive a positive integer value

--- resposta_questao_1.js
+++ resposta_questao_1.js
@@ -1,1 +1,28 @@
+const num_input = 12345678  // Variavel com o valor a ser testado
 
+function receberInteiroPositivo(valor) {
+    // Verifica se o valor é um número
+    if (typeof valor !== 'number') {
+      throw new Error('O valor deve ser um número.');
+    }
+  
+    // Verifica se o valor é inteiro
+    if (!Number.isInteger(valor)) {
+      throw new Error('O valor deve ser um inteiro.');
+    }
+  
+    // Verifica se o valor é positivo
+    if (valor <= 0) {
+      throw new Error('O valor deve ser positivo.');
+    }
+  
+    // Retorna o valor validado
+    return valor;
+}
+  
+try {
+    const numero = receberInteiroPositivo(num_input);
+    console.log('Número positivo:', numero);
+  } catch (error) {
+    console.error('Erro:', error.message);
+}
