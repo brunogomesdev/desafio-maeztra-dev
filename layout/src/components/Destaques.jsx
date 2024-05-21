d45c3b030098396165ee0622aef12ab393ac2290
brunogomesdev <brunowd@gmail.com>
2024-05-21 01:24:24 -0300

    feat(destaque): full slider banner creation

--- layout/src/components/Destaques.jsx
+++ layout/src/components/Destaques.jsx
@@ -1,9 +1,35 @@
 import '../styles/destaques.scss'
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Destaques = () => {
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+
     return (
       <div id="mz-destaques">
-        <p></p>
+        <div className="slider-container">
+          <Slider {...settings}>
+            <div className="mz-destaques-slider01">
+              <h3>Promoções de Outono</h3>
+              <p>Confiras os melhores looks para combinar com você nesse Outono</p>
+            </div>
+            <div className="mz-destaques-slider02">
+              <h3>Slider 02</h3>
+            </div>
+            <div className="mz-destaques-slider03">
+            < h3>Slider 03</h3>
+            </div>
+          </Slider>
+        </div>
       </div>
     );
   };
